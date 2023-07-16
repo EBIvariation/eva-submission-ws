b618384bc9eae01281de5fc40dbd555eeb3db9e3
sundarvenkata-ebi <sundarvenkata@ebi.ac.uk>
2023-07-16 22:24:17 +0100

    Parametrize Webin userInfo url

--- src/main/java/uk/ac/ebi/eva/submission/service/WebinTokenService.java
+++ src/main/java/uk/ac/ebi/eva/submission/service/WebinTokenService.java
@@ -1,12 +1,16 @@
 package uk.ac.ebi.eva.submission.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WebinTokenService {
+
+    @Value("${webin.userinfo.url}")
+    private String userInfoUrl;
+
     public String getWebinUserIdFromToken(String userToken) {
-        String userInfoUrl = "https://www.ebi.ac.uk/ena/submit/webin/auth/admin/submission-account";
         String tokenAttribute = "id";
-        return TokenServiceUtil.getUserId(userToken, userInfoUrl, tokenAttribute);
+        return TokenServiceUtil.getUserId(userToken, this.userInfoUrl, tokenAttribute);
     }
 }
