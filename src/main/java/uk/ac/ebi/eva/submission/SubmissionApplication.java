882ae682aa88f40b275a9ca404eca9c9c8a01c98
sundarvenkata-EBI <sundarvenkata@ebi.ac.uk>
2023-10-20 16:35:15 +0100

    Support external Tomcat deployment

--- src/main/java/uk/ac/ebi/eva/submission/SubmissionApplication.java
+++ src/main/java/uk/ac/ebi/eva/submission/SubmissionApplication.java
@@ -2,11 +2,12 @@ package uk.ac.ebi.eva.submission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class SubmissionApplication {
+public class SubmissionApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SubmissionApplication.class, args);
