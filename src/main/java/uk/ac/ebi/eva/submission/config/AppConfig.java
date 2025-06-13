4f74c75420e0d1767eef6be3945407a5b33f9fd7
nkumar2 <nkumar2@ebi.ac.uk>
2025-06-13 13:50:17 +0100

    Pass email server and port properties through application.properties

--- src/main/java/uk/ac/ebi/eva/submission/config/AppConfig.java
+++ src/main/java/uk/ac/ebi/eva/submission/config/AppConfig.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.eva.submission.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,11 +10,17 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+    @Value("${eva.email.server}")
+    private String emailServer;
+
+    @Value("${eva.email.port}")
+    private int emailPort;
+
     @Bean
     public JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost("smtp.ebi.ac.uk");
-        javaMailSender.setPort(25);
+        javaMailSender.setHost(emailServer);
+        javaMailSender.setPort(emailPort);
         return javaMailSender;
     }
 
