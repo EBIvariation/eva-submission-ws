5cd07383bb12d10e827dd596d2fe256205e6a578
tcezard <tcezard@ebi.ac.uk>
2026-05-13 10:23:13 +0100

    Enable the deprecation mechanism and update the tests to have the correct versions

--- src/main/java/uk/ac/ebi/eva/submission/exception/UnsupportedVersionException.java
+++ src/main/java/uk/ac/ebi/eva/submission/exception/UnsupportedVersionException.java
@@ -2,7 +2,7 @@ package uk.ac.ebi.eva.submission.exception;
 
 public class UnsupportedVersionException extends RuntimeException {
     public UnsupportedVersionException(String version) {
-        super("You are using deprecated version " + version + " of eva-sub-cli. " +
+        super("You are using version " + version + " of eva-sub-cli which is not supported. " +
                 "Please upgrade to the latest version to avoid future submission failures.");
     }
 }
